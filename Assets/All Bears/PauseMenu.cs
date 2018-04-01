de0f01aadc876f9388e832ab68e36e21a8f7c290
Unknown <cameron.Peet@hotmail.com>
2018-04-01 23:07:03 +1200

    Pause Menu cs script

---
+++ Assets/All Bears/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Canvas Menu;
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Start"))
+        {
+            Pause();
+        }
+    }
+	// Update is called once per frame
+	void Pause()
+    {
+	
+        if(Menu.gameObject.activeInHierarchy == false)
+        {
+            Menu.gameObject.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        else
+        {
+            Menu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+	}
+}

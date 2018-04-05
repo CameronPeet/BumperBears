9720059fd96df6f0e84d446c2ed2236aec41922d
Juan Rodríguez <timrodz@icloud.com>
2018-04-06 11:21:49 +1200

    Add visual effects folder w/ scripts, Montserrat font and TextMeshpro

---
+++ Assets/Visual Effects/DirectionalFloating.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DirectionalFloating : EffectBase
+{
+	[SerializeField]
+	public Direction direction = Direction.Both;
+
+	[SerializeField]
+	private Vector2 MoveOffset = Vector2.zero;
+
+	void Start ()
+	{
+		Animate();
+
+	}
+
+	public override void Animate ()
+	{
+		rect.DOAnchorPos (originalPosition + MoveOffset, settings.easeDuration).SetEase (settings.easeType).SetLoops (settings.loop ? -1 : 0, settings.loopType);
+	}
+
+	void OnValidate ()
+	{
+		switch (direction)
+		{
+			case Direction.Horizontal:
+				MoveOffset.y = 0;
+				break;
+			case Direction.Vertical:
+				MoveOffset.x = 0;
+				break;
+		}
+	}
+}
---
+++ Assets/Visual Effects/EffectBase.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class EffectBase : MonoBehaviour
+{
+	[SerializeField]
+	protected AnimationSettings settings;
+
+	// [SerializeField]
+	protected RectTransform rect;
+
+	protected Vector2 originalPosition;
+
+	private void Awake ()
+	{
+		rect = GetComponent<RectTransform> ();
+		originalPosition = (Vector2) rect.anchoredPosition;
+	}
+
+	void Start ()
+	{
+		if (settings.animateFromStart)
+		{
+			Animate ();
+		}
+	}
+
+	public virtual void Animate () { }
+}
+
+[System.Serializable]
+public class AnimationSettings
+{
+	public Ease easeType;
+	[Range (0.0f, 10.0f)]
+	public float easeDuration = 1.0f;
+	public bool animateFromStart = false;
+	public bool loop = false;
+	public LoopType loopType;
+}
+
+[System.Serializable]
+public enum Direction
+{
+	Horizontal,
+	Vertical,
+	Both
+};
